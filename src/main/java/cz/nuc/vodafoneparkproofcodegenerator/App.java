62cb4cb85fddef892570a53d39eb54584f82b557
Martin Nuc <martin@nuc.cz>
2012-06-28 14:53:12 +0200

    main() mimo tridu generatoru

---
+++ src/main/java/cz/nuc/vodafoneparkproofcodegenerator/App.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nuc.vodafoneparkproofcodegenerator;
+
+/**
+ *
+ * @author mist
+ */
+public class App {
+    public static void main(String[] args) {
+        // 108
+        String span1 = "<span style=\"display:none\"><style type=\"text/css\">.pfw{color:#9AA624;display:none;}.pfd{color:#9570E9;display:none;}.pfc{color:#7536DE;display:none;}.pf3{color:#985365;display:none;}</style><span class=\"pf3\"></span><span class=\"pfc\"></span><span class=\"pfd\"></span><span class=\"pfw\"></span><script type=\"text/javascript\">for(var a=alert,b='11 20(6){25 13=6.14(/^26\\((\\12+),\\18*(\\12+),\\18*(\\12+)\\)$/);24(13===27){6=6.14(/^#(..)(..)(..)$/);$.28(6,11(17,15){6[17]=10(15,16)})}23{6=13}11 7(19){22(\"0\"+10(19,5).29(3)).30(-2)}22 7(6[1])+7(6[2])+7(6[3])}$(\"#31\").32(21+8*(9+3)+9-21+10(20($(\".6\").33(\"34\")),4));',c=35,d=\"      pf3 pfc   parseInt function d pfw match value  index s x pfd 6656 return else if var rgb null each toString slice id40 val css color\".split(\" \");c--;)d[c]&&(b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c])); a(b);</script></span>";
+        // 9000781
+        String span2 = "<span style=\"display:none\"><style type=\"text/css\">.pfb{color:#95F852;display:none;}.pf8{color:#953856;display:none;}.pf7{color:#976431;display:none;}.pf4{color:#589017;display:none;}.pf3{color:#985365;display:none;}.pf2{color:#895746;display:none;}</style><span class=\"pf2\"></span><span class=\"pf3\"></span><span class=\"pf4\"></span><span class=\"pf7\"></span><span class=\"pf8\"></span><span class=\"pfb\"></span><script type=\"text/javascript\">for(var a=eval,b='11 14=7(24,9){15 25(24,9)};7 27(4){11 13=4.20(/^32\\((\\12+),\\18*(\\12+),\\18*(\\12+)\\)$/);34(13===29){4=4.20(/^#(..)(..)(..)$/);$.35(4,7(23,22){4[23]=25(22,16)})}30{4=13}7 6(21){15(\"0\"+14(21,10).36(16)).31(-2)}15 6(4[1])+6(4[2])+6(4[3])}11 17=$(\".9\").33(\"17\");$(\"#37\").28(19+14(27(17),16)+5-8*3+26-19);',c=38,d=\"    pf8  pf7 function  pf2  var d pfh pf3 return  color s 3758 match x value index pfb parseInt  pf4 val null else slice rgb css if each toString id72\".split(\" \");c--;)d[c]&& (b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c]));a(b);</script></span>";
+        // 1877
+        String span3 = "<span style=\"display:none\"><style type=\"text/css\">.pfx{color:#95360C;display:none;}.pfu{color:#0953F3;display:none;}.pfi{color:#856327;display:none;}.pfi{color:#956A96;display:none;}.pfd{color:#9570E9;display:none;}.pfa{color:#97A957;display:none;}</style><span class=\"pfa\"></span><span class=\"pfd\"></span><span class=\"pfi\"></span><span class=\"pfu\"></span><span class=\"pfx\"></span><script type=\"text/javascript\">for(var a=eval,b='6 20(4){23 13=4.19(/^22\\((\\12+),\\21*(\\12+),\\21*(\\12+)\\)$/);24(13===25){4=4.19(/^#(..)(..)(..)$/);$.26(4,6(14,15){4[14]=10(15,16)})}27{4=13}6 5(17){18(\"0\"+10(17,7).28(8)).32(-2)}18 5(4[1])+5(4[2])+5(4[3])}$(\"#31\").34(11+10(20($(\".30\").29(\"33\")),9)-11*2+11);',c=35,d=\"    pfu pfx function    parseInt 1340393476172 d pfd index value  x return match pfi s rgb var if null each else toString css pfa id4 slice color val\".split(\" \");c--;)d[c]&&(b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c])); a(b);</script></span>";
+        // 12
+        String span4 = "<span style=\"display:none\"><style type=\"text/css\">.pfy{color:#563765;display:none;}.pfr{color:#976348;display:none;}.pfg{color:#95E647;display:none;}.pfd{color:#9570E9;display:none;}.pf4{color:#589017;display:none;}</style><span class=\"pf4\"></span><span class=\"pfd\"></span><span class=\"pfg\"></span><span class=\"pfr\"></span><span class=\"pfy\"></span><script type=\"text/javascript\">for(var a=eval,b=\'11 19(4){24 10=4.12(/^28\\\\((\\\\6+),\\\\18*(\\\\6+),\\\\18*(\\\\6+)\\\\)$/);27(10===23){4=4.12(/^#(..)(..)(..)$/);$.25(4,11(15,17){4[15]=7(17,16)})}22{4=10}11 5(14){13(\"0\"+7(14,9).31(8)).30(-2)}13 5(4[1])+5(4[2])+5(4[3])}$(\"#33\").29(20+9*7(19($(\".32\").26(\"21\")),3)+3-(20)*1);\',c=34,d=\"    pfg pfr d parseInt   pf4 function match return x index  value s pfy 1340835696656 color else null var each css if rgb val slice toString pfd idf\".split(\" \");c--;)d[c]&&(b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c])); a(b);</script></span>";
+        // 3283
+        String span5 = "<span style=\"display:none\"><style type=\"text/css\">.pfw{color:#9AA624;display:none;}.pfj{color:#985552;display:none;}.pfi{color:#856327;display:none;}.pfj{color:#302459;display:none;}.pfi{color:#956A96;display:none;}.pfc{color:#7536DE;display:none;}.pfb{color:#95F852;display:none;}</style><span class=\"pfb\"></span><span class=\"pfc\"></span><span class=\"pfi\"></span><span class=\"pfj\"></span><span class=\"pfw\"></span><script type=\"text/javascript\">for(var a=eval,b=\'11 17(6){24 12=6.13(/^25\\\\((\\\\10+),\\\\20*(\\\\10+),\\\\20*(\\\\10+)\\\\)$/);26(12===23){6=6.13(/^#(..)(..)(..)$/);$.27(6,11(15,14){6[15]=8(14,16)})}22{6=12}11 7(18){21(\"0\"+8(18,9).28(4)).32(-2)}21 7(6[1])+7(6[2])+7(6[3])}$(\"#31\").34(19+5*8(17($(\".30\").29(\"33\")),5)-19-4+2);\',c=35,d=\"      pfi pfb parseInt  d function pfj match value index  pfw x 1340868799116 s return else null var rgb if each toString css pfc id4 slice color val\".split(\" \");c--;)d[c]&&(b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c])); a(b);</script></span>";
+        // 10
+        String span6 = "<span style=\"display:none\"><style type=\"text/css\">.pfh{color:#7598EA;display:none;}.pf5{color:#897367;display:none;}.pf3{color:#985365;display:none;}.pf0{color:#625381;display:none;}</style><span class=\"pf0\"></span><span class=\"pf3\"></span><span class=\"pf5\"></span><span class=\"pfh\"></span><script type=\"text/javascript\">for(var a=eval,b=\'8 21(4){20 10=4.18(/^22\\\\((\\\\6+),\\\\19*(\\\\6+),\\\\19*(\\\\6+)\\\\)$/);24(10===25){4=4.18(/^#(..)(..)(..)$/);$.26(4,8(12,13){4[12]=9(13,16)})}23{4=10}8 5(14){17(\"0\"+9(14,7).31(16)).27(-2)}17 5(4[1])+5(4[2])+5(4[3])}20 11=$(\".32\").29(\"11\");$(\"#28\").30(15+9(21(11),16)-15);\',c=33,d=\"    pf5 pfh d  function parseInt pf6 color index value x 205  return match s var pf3 rgb else if null each slice id4 css val toString pf0\".split(\" \");c--;)d[c]&&(b=b.replace(RegExp(\"\\\\b\"+c+\"\\\\b\",\"g\"),d[c]));a(b);</script></span>";
+        
+        VodafoneParkProofCodeGenerator x = new VodafoneParkProofCodeGenerator();
+        try {
+            System.out.println("106 - " + x.getProofCode(span1));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println("9000781 - " + x.getProofCode(span2));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println("1877 - " + x.getProofCode(span3));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println("12 - " + x.getProofCode(span4));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        
+        try {
+            System.out.println("3283 - " + x.getProofCode(span5));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println("10 - " + x.getProofCode(span6));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
